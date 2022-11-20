659e9323d844562f0e7418cd83e950bf74385f83
PavelOsw <pavel.olage@alumnos.udg.mx>
2022-11-19 19:39:13 -0600

    Ventana de filtros

--- gui.py
+++ gui.py
@@ -1,27 +1,50 @@
 from tkinter import *
 from tkinter import ttk, filedialog
-import pandas as pd
+from PIL import ImageTk, Image
 
-root = Tk()
-root.config(width=850, height=400)
-root.title("Ferretería")
+# from filtros import win2
+
+win1 = Tk()
+win1.config(width=400, height=500)
+win1.title("Ferretería")
 
 
-frame = Frame(root)
+frame = Frame(win1)
 frame.pack(pady = 5)
 frame.place(x = 25, y = 50)
 
 vista = ttk.Treeview(frame)
 
-ttk.Label(root, text = "Selecciona filtro: ").place(x=25, y=5)
+# ttk.Label(win1, text = "Ferretaría").place(x=25, y=5)
 
-# menuFiltro = ttk.Combobox(root, state="readonly", values=["", ""])
+img_1 = Image.open("img/logo_2.png")
+img_1 = img_1.resize((150, 150), Image.ANTIALIAS)
+test = ImageTk.PhotoImage(img_1)
+logo1 = ttk.Label(image=test)
+logo1.image = test
+logo1.place(x=25, y=5)
+
+# menuFiltro = ttk.Combobox(win1, state="readonly", values=["", ""])
 # menuFiltro.place (x =25, y = 25)
 
-btnFiltro = ttk.Button(root, text = "Filtrar", command='')
-btnFiltro.place(x=25, y= 300)
+def clear():
+    vista.delete(*vista.get_children())
 
-btnTabla = ttk.Button(root, text = "Todo", command='')
-btnTabla.place(x=150, y= 300)
+# def win_reportes():
+#     clear()
+#     # win2
 
-root.mainloop()
+btnAccion1 = ttk.Button(win1, text = "Base de datos", command='')
+btnAccion1.place(x=150, y= 200)
+
+btnAccion2 = ttk.Button(win1, text = "Reportes", command='win_reportes')
+btnAccion2.place(x=150, y= 300)
+
+btnSalir = ttk.Button(win1, text = "Salir", command='exit')
+btnSalir.place(x=150, y= 400)
+
+
+def exit():
+    win1.destroy
+
+win1.mainloop()
